d53e48bb45d33c94f641ffd1ad93d28b39b7a29f
clipmagic <admin@clipmagic.com.au>
2026-07-27 14:25:40 +1000

    add vendor lbuchs to git

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'clipmagic/loginpasskey',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '97bd93a29f9b12cbe74c2baf5a83f19ce0cd9400',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'clipmagic/loginpasskey' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '97bd93a29f9b12cbe74c2baf5a83f19ce0cd9400',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'lbuchs/webauthn' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => '20adb4a240c3997bd8cac7dc4dde38ab0bea0ed1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../lbuchs/webauthn',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
